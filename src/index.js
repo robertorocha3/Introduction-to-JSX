c744af99c950d07e1f63c823d571742ffccf0699
robertorocha3 <robertorocha3@gmail.com>
2021-09-29 21:14:00 +1300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>this is a paragraph</p>
+  </div>,
+  document.getElementById("root")
+);
